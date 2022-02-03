ff6782d1c42b907b42bcad2302d6110f1c053cd6
Marcus Balbi <marcus.balbi@olivia.ai>
2022-02-03 16:50:13 -0300

    improve fn valor-descontado

--- clojure1/basic-examples.clj
+++ clojure1/basic-examples.clj
@@ -7,3 +7,26 @@
         desconto        (* valor-bruto taxa-desconto)]
         (println "Calculando desconto de " desconto)
         (- valor-bruto desconto)))
+
+
+(if (> 500 100) 
+(println "500 e maior que 100")
+(println "500 não é maior que 100"))
+
+(defn eh-maior
+    "Verifica se o primeiro numero e maior que o segundo"
+    [n1, n2]
+    (if (> n1, n2)
+        (println "O número " n1 "é maior do que " n2)
+        (println "O número " n1 "não é maior do que " n2)))
+
+
+(defn valor-descontado
+    "Retornar o valor com desconto de 10%"
+    [valor-bruto]
+    (if (> valor-bruto 100)
+        (let [taxa-desconto (/ 10 100)
+            desconto        (* valor-bruto taxa-desconto)]
+            (println "Calculando desconto de " desconto)
+            (- valor-bruto desconto))
+        valor-bruto))
