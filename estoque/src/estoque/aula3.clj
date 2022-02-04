64ce3e6f7c6d4b492efaceed660e600482a85590
Marcus Balbi <marcus.balbi@olivia.ai>
2022-02-03 21:14:46 -0300

    aula 3

--- estoque/src/estoque/aula3.clj
+++ estoque/src/estoque/aula3.clj
@@ -1,20 +1,21 @@
 (ns estoque.aula3)
-
-
-(defn aplica-desconto?
+(defn maior-que-100?
   [valor-bruto]
   (> valor-bruto 100))
 
 (defn valor-descontado
   "Retornar o valor com desconto de 10%"
-  [valor-bruto]
-  (if (aplica-desconto? valor-bruto)
+  [aplica?, valor-bruto]
+  (if (aplica? valor-bruto)
     (let [taxa-desconto (/ 10 100)
           desconto (* valor-bruto taxa-desconto)]
       (- valor-bruto desconto))
     valor-bruto))
 
-(println (valor-descontado 500))
-(println (valor-descontado 90))
-(println (aplica-desconto? 150))
-(println (aplica-desconto? 80))
+(println (valor-descontado maior-que-100?, 500))
+(println (valor-descontado maior-que-100?, 90))
+
+;; funcao anonima
+(println (valor-descontado (fn [v] (> v 50)) 60))
+(println (valor-descontado #(> %1 30) 100))
+(println (valor-descontado #(> % 30) 100))
