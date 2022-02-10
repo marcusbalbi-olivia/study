bb4a0ce4b97bd4b0662f708e46d4d3d92a236589
Marcus Balbi <marcus.balbi@nubank.com.br>
2022-02-09 22:38:01 -0300

    aula 3

---
+++ loja/src/loja/aula3.clj
@@ -0,0 +1,62 @@
+(ns loja.aula3
+  (:require [loja.db :as l.db]))
+
+(println (l.db/todos-os-pedidos))
+
+(println (group-by :usuario (l.db/todos-os-pedidos)))
+
+(defn minha-funcao-agrupamento
+  [elemento]
+  (println elemento)
+  (:usuario elemento))
+
+(println (group-by minha-funcao-agrupamento (l.db/todos-os-pedidos)))
+
+
+(println (map count (vals (group-by :usuario   (l.db/todos-os-pedidos)))))
+
+
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     vals
+     (map count)
+     println)
+
+
+(defn conta-total-por-usuario
+  [[usuario, pedidos]]
+  { :usuario usuario :qtde-itens (count pedidos)}
+  )
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+(defn total-do-item [[chave detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0 )))
+
+(defn total-do-pedido [pedido]
+  (reduce + (map total-do-item pedido)))
+
+(defn total-dos-pedidos
+  [pedidos]
+  (->> pedidos
+       (map :itens)
+       (map total-do-pedido)
+       (reduce +)))
+
+(defn quantia-de-pedidos-e-gasto-total-por-usuario
+  [[usuario, pedidos]]
+  { :usuario usuario
+    :total-pedido (count pedidos)
+    :preco-total (total-dos-pedidos pedidos)
+   }
+  )
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map quantia-de-pedidos-e-gasto-total-por-usuario) println)
+
+;(println (total-item {:quantidade 2 :preco-unitario 80}))
---
+++ loja/src/loja/db.clj
@@ -0,0 +1,47 @@
+(ns loja.db)
+
+(def pedido1 {
+              :usuario 15
+              :itens {
+                      :mochila { :id :mochila :quantidade 2, :preco-unitario 80 }
+                      :camiseta { :id :camiseta :quantidade 2, :preco-unitario 40 }
+                      :tenis { :id :tenis :quantidade 1 }
+                      }
+              })
+
+(def pedido2 {
+              :usuario 18
+              :itens {
+                      :mochila { :id :mochila :quantidade 1, :preco-unitario 80 }
+                      :camiseta { :id :camiseta :quantidade 1, :preco-unitario 40 }
+                      }
+              })
+
+(def pedido3 {
+              :usuario 20
+              :itens {
+                      :mochila { :id :mochila :quantidade 1, :preco-unitario 80 }
+                      :camiseta { :id :camiseta :quantidade 6, :preco-unitario 40 }
+                      :tenis { :id :tenis :quantidade 1 }
+                      }
+              })
+
+(def pedido4 {
+              :usuario 22
+              :itens {
+                      :mochila { :id :mochila :quantidade 3, :preco-unitario 80 }
+                      :camiseta { :id :camiseta :quantidade 1, :preco-unitario 40 }
+                      :tenis { :id :tenis :quantidade 1 }
+                      }
+              })
+
+(def pedido5 {
+              :usuario 15
+              :itens {
+                      :mochila { :id :mochila :quantidade 1, :preco-unitario 80 }
+                      :camiseta { :id :camiseta :quantidade 1, :preco-unitario 40 }
+                      }
+              })
+
+(defn todos-os-pedidos []
+  [pedido1 pedido2 pedido3 pedido4 pedido5])
