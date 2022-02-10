cdef2bd211fe7a639f4ee393ce1b0aa874c3ed9a
Marcus Balbi <marcus.balbi@nubank.com.br>
2022-02-09 21:05:02 -0300

    aula2 reduce

---
+++ loja/src/loja/aula2.clj
@@ -0,0 +1,30 @@
+(ns loja.aula2)
+
+; ["Maria" "Pedro" "Augusto" "Fabiana"]
+
+;(defn conta
+;  [total-ate-agora elementos]
+;  (recur (inc total-ate-agora) (rest elementos)))
+
+
+(defn minha-funcao
+  "TESTE assinatura"
+  ([p1] (println p1))
+  ([] (println "No parameters"))
+  )
+
+(minha-funcao)
+(minha-funcao "TESTE 1 Parametro")
+
+(defn conta
+
+  ([elementos]
+   (conta 0 elementos))
+
+  ([total-ate-agora elementos]
+  (if (seq elementos)
+    (recur (inc total-ate-agora) (next elementos))
+    total-ate-agora)))
+
+(println (conta 0 ["Maria" "Pedro" "Augusto" "Fabiana"]))
+(println (conta ["Maria" "Pedro" "Augusto" "Fabiana"]))
