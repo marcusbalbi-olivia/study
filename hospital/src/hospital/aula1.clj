74e857a17bc281adf2aef80056fe1edee5c91f66
Marcus Balbi <marcus.balbi@nubank.com.br>
2022-02-14 17:51:17 -0300

    thread problem

--- hospital/src/hospital/aula1.clj
+++ hospital/src/hospital/aula1.clj
@@ -10,6 +10,8 @@
   (def hospital (h.logic/chega-em hospital :espera "222"))
   (def hospital (h.logic/chega-em hospital :espera "333"))
   (def hospital (h.logic/chega-em hospital :espera "444"))
+  (def hospital (h.logic/chega-em hospital :espera "888"))
+  (def hospital (h.logic/chega-em hospital :espera "999"))
   (pprint hospital)
   (def hospital (h.logic/chega-em hospital :laboratorio1 "555"))
   (def hospital (h.logic/chega-em hospital :laboratorio3 "777"))
@@ -20,4 +22,24 @@
   )
 
 
-(simula-um-dia)
+;(simula-um-dia)
+
+(defn chega-em-malvado [pessoa]
+  (def hospital (h.logic/chega-em-pausado hospital :espera pessoa))
+  (println "apos inserir" pessoa))
+
+(defn simula-um-dia-em-paralelo []
+  ; Simula o problema de variavel compartilhada em um ambiente multithread
+  (def hospital (h.model/novo-hospital))
+  (.start (Thread. (fn [] (chega-em-malvado "111"))))
+  (.start (Thread. (fn [] (chega-em-malvado "222"))))
+  (.start (Thread. (fn [] (chega-em-malvado "333"))))
+  (.start (Thread. (fn [] (chega-em-malvado "444"))))
+  (.start (Thread. (fn [] (chega-em-malvado "555"))))
+  (.start (Thread. (fn [] (chega-em-malvado "666"))))
+  (.start (Thread. (fn []
+                           (Thread/sleep 4000)
+                           (pprint hospital)
+                            ))))
+
+(simula-um-dia-em-paralelo)
--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -1,8 +1,26 @@
 (ns hospital.logic)
 
-(defn chega-em [hospital departamento pessoa]
-  (update hospital departamento conj pessoa))
+(defn cabe-na-fila? [hospital departamento]
+  (-> hospital
+      (get ,,, departamento)
+      count,,,
+      (<,,, 5)))
 
+;(defn cheio2? [hospital departamento]
+;  (let [fila (get hospital departamento)
+;        tamanho-atual (count fila)]
+;    (>= tamanho-atual 5)))
+
+(defn chega-em [hospital departamento pessoa]
+    (if (cabe-na-fila? hospital departamento)
+      (update hospital departamento conj pessoa)
+      (throw (ex-info "Fila já está cheia!" { :tentando-adicionar pessoa }))))
+
+(defn chega-em-pausado [hospital departamento pessoa]
+  (if (cabe-na-fila? hospital departamento)
+    (do (Thread/sleep (* (rand) 2000))
+        (update hospital departamento conj pessoa))
+    (throw (ex-info "Fila já está cheia!" { :tentando-adicionar pessoa }))))
 
 (defn atende [hospital departamento]
   (update hospital departamento pop))
