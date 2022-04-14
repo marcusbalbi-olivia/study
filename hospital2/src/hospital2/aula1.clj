9b0f8da5a1774c24417164d5bf81d3f5bba84303
Marcus Balbi <marcus.balbi@nubank.com.br>
2022-04-13 22:05:16 -0300

    finish aula1

--- hospital2/src/hospital2/aula1.clj
+++ hospital2/src/hospital2/aula1.clj
@@ -20,3 +20,29 @@
     ))
 
 (clojure.pprint/pprint (teste-pacientes))
+
+
+;(defrecord Paciente [^Long id ^String nome nascimento])
+(defrecord Paciente [id nome nascimento])
+
+(clojure.pprint/pprint (->Paciente 3 "Julia Balbi" "25/11/2019"))
+
+(Paciente. 4 "Fabiano" "10/10/2010")
+(Paciente. "Fabiano" 52 "10/10/2010")
+
+(map->Paciente {:id 15 :nome "Daniel" :nascimento "24/04/1985"})
+
+(let [balbi (->Paciente 1 "Balbi" "27/07/1989")]
+  (println (:id balbi))
+  (println (vals balbi))
+  (println (into balbi { :rg "12345679" }))
+  (println (assoc balbi :id 25))
+  (class balbi)
+  (record? balbi)
+  (println (.nome balbi)))
+
+;(Paciente. "Fabiano" "10/10/2010")
+
+(= (->Paciente 1 "Balbi" "27/07/1989") (->Paciente 1 "Balbi" "27/07/1989"))
+(= (->Paciente 1 "Balbi" "27/07/1989") (->Paciente 1 "Balbi" "27/07/189"))
+
