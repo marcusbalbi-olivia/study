7d41c6eaa21ba2ddf3a9c08a5eb3131a3cb87937
Marcus Balbi <marcus.balbi@nubank.com.br>
2022-04-22 19:45:49 -0300

    finish aula2

--- hospital3/src/hospital3/aula2.clj
+++ hospital3/src/hospital3/aula2.clj
@@ -21,21 +21,13 @@
 ;; (s/validate Paciente { :id 20 :nome 33 })
 ;; (s/validate Paciente {:id 20 :nome "Joao" :endereco {:logradoutro "Teste" :cep "28625530"} :plano [:raio-x :coleta-de-sangue]})
 
-(def Paciente
-  "Schema de um Paciente"
-  {:id s/Num :nome s/Str})
+;; (def Paciente
+;;   "Schema de um Paciente"
+;;   {:id s/Num :nome s/Str})
 
 ;; (s/defn novo-paciente :- Paciente
 ;;         [id :- s/Num nome :- s/Str]
 ;;         {:id id :nome nome :plano [] })
-
-(s/defn novo-paciente :- Paciente
-  [id :- s/Num nome :- s/Str]
-  {:id id :nome nome })
-
-(novo-paciente 15 "Balbi")
-
-
 (defn estritamente-positivo?  [x]
   (> x 0))
 
@@ -45,3 +37,15 @@
 (s/validate EstritamentePositivo -20)
 (s/validate EstritamentePositivo 0)
 (s/validate EstritamentePositivo "Not a number")
+
+(def Paciente
+  "Schema de um Paciente"
+  {:id (s/constrained s/Int pos?) :nome s/Str})
+
+(s/defn novo-paciente :- Paciente
+  [id :- s/Num nome :- s/Str]
+  {:id id :nome nome })
+
+(novo-paciente 15 "Balbi")
+(novo-paciente -15 "Balbi")
+
