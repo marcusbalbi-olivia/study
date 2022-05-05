f86b76fe83ef4a38cf504d4e21dd51c62b5a24bb
Marcus Balbi <marcus.balbi@nubank.com.br>
2022-05-05 13:37:39 -0300

    nu clojure

--- experiments/src/experiments/core.clj
+++ experiments/src/experiments/core.clj
@@ -1,7 +1,49 @@
 (ns experiments.core
   (:gen-class))
 
-(rep Year)
+(def colors ["red" "green"])
+(println colors)
+(println (conj colors "blue"))
+(println colors)
+(println (into colors ["blue"]))
+(println (assoc colors (count colors) "blue"))
+
+;; not pure
+(defn hello-world []
+  "provides an hour-appropriate greeting string"
+  (let [now (java.time.LocalTime/now)]
+    (if (< (.getHour now) 12)
+      (println "Good Morning")
+      (println "Good Afternoon"))))
+(hello-world)
+
+;; pure
+(defn greetings [hour]
+  "provides an hour-appropriate greeting string"
+  (if (< hour 12)
+    "Good Morning"
+    "Good Afternoon"))
+
+(greetings 10)
+(greetings 13)
+
+
+(defn greetings-improved [hour]
+  "provides an hour-appropriate greeting string"
+
+  (cond
+    (< hour 12)  "Good Morning"
+    (< hour 17)  "Good Afternoon"
+    (< hour 20)  "Good Evening"
+    (>= hour 21) "Good Night"))
+
+(greetings 10)
+(greetings 13)
+(greetings 18)
+(greetings 23)
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
